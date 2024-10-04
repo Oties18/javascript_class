f746d8892da9e79a48e02ffe95118d93dfe8a018
=Oluwatamilore Ibrahim <=tamiloreibrahim@gmail.com>
2024-10-04 12:08:09 +0100

    Add new js code and indent properly

--- index.js
+++ index.js
@@ -10,12 +10,21 @@ Guess = prompt("Guess the number")
 if (Guess == 18){
     console.log("Correct!")
 }
-    else if (Guess <= 12){
+else if (Guess <= 12){
        console.log("Too low, Go higher") 
     }
-        else if(Guess >= 19){
+else if(Guess >= 19){
             console.log("Too high, Go lower")
         }
 else {
     console.log("Wrong!")
+}
+
+let numberToGuess = 18;
+Guess = prompt("Guess the number");
+if (Guess == numberToGuess){
+    console.log("Correct!")
+}
+else {
+    console.log("Wrong!");
 }
