0feed14b917ddfe6f980a32957b3cb99e5e00ef7
=Oluwatamilore Ibrahim <=tamiloreibrahim@gmail.com>
2024-10-04 13:43:05 +0100

    Select timestable from 2 - 6

--- index.js
+++ index.js
@@ -33,3 +33,34 @@ for(var i= 1; i <= 12; i++){
     
     console.log("2" + " " + "x" + " " + "i" + " " + "=" + " " + 2 * i)
 }
+
+var Table = prompt("Select a timestable");
+if (Table == 2) {
+    for(var i= 1; i <= 12; i++){
+    
+    console.log("2" + " " + "x" + " " + "i" + " " + "=" + " " + 2 * i)
+}
+}
+else if (Table == 3) {
+    for(var i= 1; i <= 12; i++){
+    
+    console.log("3" + " " + "x" + " " + "i" + " " + "=" + " " + 3 * i)
+}
+}
+else if (Table == 4) {
+    for(var i= 1; i <= 12; i++){
+    
+    console.log("4" + " " + "x" + " " + "i" + " " + "=" + " " + 4 * i)
+}
+}else if (Table == 5) {
+    for(var i= 1; i <= 12; i++){
+    
+    console.log("5" + " " + "x" + " " + "i" + " " + "=" + " " + 5 * i)
+}
+}
+else if (Table == 6) {
+    for(var i= 1; i <= 12; i++){
+    
+    console.log("6" + " " + "x" + " " + "i" + " " + "=" + " " + 6 * i)
+}
+}
