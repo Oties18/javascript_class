3dea7633f0f8f3f8a2fd5376b9a691d9cf7a4381
=Oluwatamilore Ibrahim <=tamiloreibrahim@gmail.com>
2024-10-04 13:49:18 +0100

    Add new comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// prompt code to guess correct number
 Guess = prompt("Guess the number")
 if (Guess == 18){
     console.log("Correct!")
@@ -6,6 +7,8 @@ else {
     console.log("Wrong!")
 }
 
+
+// prompt code to guess correct number and tell if user should go lower or higher
 Guess = prompt("Guess the number")
 if (Guess == 18){
     console.log("Correct!")
@@ -20,6 +23,7 @@ else {
     console.log("Wrong!")
 }
 
+// prompt code to guess correct number
 let numberToGuess = 18;
 Guess = prompt("Guess the number");
 if (Guess == numberToGuess){
@@ -29,11 +33,14 @@ else {
     console.log("Wrong!");
 }
 
+// for loop that prints out 2 timestable from 1 to 12
 for(var i= 1; i <= 12; i++){
     
     console.log("2" + " " + "x" + " " + "i" + " " + "=" + " " + 2 * i)
 }
 
+
+// for loop that prints out 2 - 6 timestable from 1 to 12
 var Table = prompt("Select a timestable");
 if (Table == 2) {
     for(var i= 1; i <= 12; i++){
