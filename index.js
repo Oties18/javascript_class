e3da278976f4fa55a37ad574888b8363d1892e12
=Oluwatamilore Ibrahim <=tamiloreibrahim@gmail.com>
2024-10-07 08:22:53 +0100

    Add FizzBuzz

--- index.js
+++ index.js
@@ -71,3 +71,17 @@ else if (Table == 6) {
     console.log("6" + " " + "x" + " " + "i" + " " + "=" + " " + 6 * i)
 }
 }
+
+// FizzBuzz
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
